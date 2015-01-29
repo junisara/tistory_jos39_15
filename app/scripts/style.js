3655fc73c6179d13d4ee5ce57e84b506adb7134b
git <junisara@gmail.com>
2015-01-30 01:02:31 +0900

    .

--- app/scripts/style.js
+++ app/scripts/style.js
@@ -1,25 +1,24 @@
-
-
-jQuery(function() {
-
-		jQuery(".gnb .depth1")
-
-});
-
-jQuery(".gnb .depth1").hover(function() {
-
-
-  	if ( jQuery( ".gnb .depth1 ul" ).is( ":hidden" ) ) {
-
-     jQuery( ".gnb .depth1 ul" ).css( "display","block" ).slideDown( "slow" );
-
-  } else {
-
-    jQuery( ".gnb .depth1 ul" ).css( "display","block" ).slideUp( "slow" );
-
-  }
+'use strict';
 
 
 
+
+
+
+
+var delay = 150,
+    setTimeoutConst;
+
+jQuery('.gnb .depth1').hover(function() {
+
+    setTimeoutConst = setTimeout(function() {
+        jQuery('.gnb .depth1 ul').slideDown('slow').css('display', 'block');
+    }, delay);
+
+}, function() {
+    clearTimeout(setTimeoutConst);
+    if (jQuery('.gnb .depth1 ul').is(':hidden')) {} else {
+        jQuery('.gnb .depth1 ul').css('display', 'block').slideUp('slow');
+    }
 
 });
